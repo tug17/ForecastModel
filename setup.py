264b877774315b8d25530f8c9fe4b6ce00c1687e
tug17 <78030747+tug17@users.noreply.github.com>
2023-11-29 10:05:46 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ForecastModel',
+    version = '0.1.0',
+    url = 'https://github.com/tug17/ForecastModel',
+    description = '',
+    packages = find_packages(),
+    install_requires = [
+    ]
+)
