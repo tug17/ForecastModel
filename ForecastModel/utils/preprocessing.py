51edd23e11bc25bf5d153cde8cb0e83613f0bbce
tug17 <78030747+tug17@users.noreply.github.com>
2024-03-04 13:12:41 +0100

    update readme 1.0
    add yml file

--- ForecastModel/utils/preprocessing.py
+++ ForecastModel/utils/preprocessing.py
@@ -19,6 +19,7 @@ class CreateIndices:
         # load data
         self.df = pd.read_csv(data_path, parse_dates=['time'], index_col='time')
         self.df = self.df.set_index(pd.to_datetime(self.df.index))
+        self.df["n_idx"] = np.arange(self.df.shape[0], dtype=int)
         try: 
             self.mask = self.df["is_peak_flow"].values.tolist()
         except:
@@ -54,41 +55,62 @@ class CreateIndices:
         return X, y
 
     #%%
-    def create(self, n_sets = 7, hincast_lengths = [12, 24, 36, 48, 60, 72, 84, 96, 108, 120], forecast_len = 96, target_len = 96, oscilation_len=0):
+    def create(self, n_sets = 7, hincast_lengths = [12, 24, 36, 48, 60, 72, 84, 96, 108, 120], forecast_len = 96, target_len = 96, oscilation_len=0, dtime_secs=15*60):
         for hincast_len in hincast_lengths:
             print("prepareing index arrays")
             
-            n_preCutOff  = hincast_len
-            n_postCutOff = target_len
+            hindcast_delta  = hincast_len * timedelta(seconds=dtime_secs)
+            forecast_delta  = forecast_len* timedelta(seconds=dtime_secs)
+            target_delta    = target_len  * timedelta(seconds=dtime_secs)
+            
+            start_date = self.df.index[0]  + hindcast_delta
+            end_date   = self.df.index[-1] # - np.max((forecast_delta, target_delta))
             
             
-            n_samples = self.df.shape[0] - n_preCutOff
+            i_splits = [(start_date + x * (end_date-start_date) / n_sets).round('1d') for x in range(0,n_sets+1)]
             
-            i_splits = [n_preCutOff + int(x*n_samples/n_sets) for x in range(0,n_sets+1)]
+            df_part = self.df.loc[self.mask]
             
             sets = {}
             for n_set, i in enumerate(range(1,n_sets+1)):
-                i_set = np.arange(i_splits[i-1], i_splits[i] - n_postCutOff, dtype=int)
+                print(f"processing set {n_set+1}")
+                set_mask = (df_part.index > i_splits[i-1]) & (df_part.index < i_splits[i] - forecast_delta)
+                df_part_set = df_part.loc[set_mask]
                 
-                # masking
-                i_set = np.array([x for x in i_set.tolist() if self.mask[x]])
+                #set_mask = (self.df.index > i_splits[i-1]) & (self.df.index <= i_splits[i] - forecast_delta)
+                #df_set = self.df.loc[set_mask]
                 
-                i_hincast  = np.zeros((len(i_set), hincast_len, 1), dtype=int)
-                i_forecast = np.zeros((len(i_set), forecast_len + oscilation_len, 1), dtype=int)
-                i_target   = np.zeros((len(i_set), target_len   + oscilation_len, 1), dtype=int)
-                for n,j in enumerate(i_set):
-                    i_hincast[n,:,0]  = np.arange(j - hincast_len, 
-                                                  j, 
-                                                  dtype=int)
-                    i_forecast[n,:,0] = np.arange(j - oscilation_len + 1, 
-                                                  j + forecast_len + 1, 
-                                                  dtype=int)
-                    i_target[n,:,0]   = np.arange(j - oscilation_len + 1, 
-                                                  j + target_len   + 1, 
-                                                  dtype=int)
+                n_samples = df_part_set.shape[0]
                 
-                if np.max(i_forecast) != i_splits[i] - 1:
-                    print(f"index error: {np.max(i_forecast)} / {i_splits[i] - 1}")
+                i_hincast  = np.zeros((n_samples, hincast_len, 1), dtype=int)
+                i_forecast = np.zeros((n_samples, forecast_len + oscilation_len, 1), dtype=int)
+                i_target   = np.zeros((n_samples, target_len   + oscilation_len, 1), dtype=int)
+                
+                for n,date in enumerate(df_part_set.index):
+                    hindcast_dates = pd.date_range(date - hindcast_delta, date, freq=f"{dtime_secs:d}s")[1:]
+                    forecast_dates = pd.date_range(date, date + forecast_delta, freq=f"{dtime_secs:d}s")[1:]
+                    target_dates   = pd.date_range(date, date + target_delta,   freq=f"{dtime_secs:d}s")[1:]
+                    
+                    if forecast_dates[-1] > i_splits[i]:
+                        print("set limit reached")
+                        np.delete(i_hincast,  n, axis=0)
+                        np.delete(i_forecast, n, axis=0)
+                        np.delete(i_target,   n, axis=0)
+                        continue
+                    
+                    try:
+                        i_hincast[n,:,0]  = self.df.loc[hindcast_dates, "n_idx"].values
+                        i_forecast[n,:,0] = self.df.loc[forecast_dates, "n_idx"].values
+                        i_target[n,:,0]   = self.df.loc[target_dates,   "n_idx"].values
+                    except:
+                        print(f"an error occured in sample {n}: sample was removed")
+                        np.delete(i_hincast,  n, axis=0)
+                        np.delete(i_forecast, n, axis=0)
+                        np.delete(i_target,   n, axis=0)
+                    
+                
+                if np.max(i_forecast) >= self.df["n_idx"].values[-1]:
+                    print(f"index error: {np.max(i_forecast)}")
                 if np.min(i_hincast) < 0:
                     print("index error: index below 0")
             
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# ForecastModel
- Hindcast-CNN-LSTM-model for improvment of hydrological forecasts
+# Hindcast-LSTM-model for improvment of hydrological forecasts
+Accompanying code for our HESS paper "Long Short-Term Memory Networks for Real-time Flood Forecast Correction: A Case Study for an Underperforming Hydrologic Model"
+
+```
+Gegenleithner, S., Manuel Pirker, M., Dorfmann, C., Kern, R., and Schneider, J.: Long Short-Term Memory 
+Networks for Real-time Flood Forecast Correction: A Case Study for an Underperforming Hydrologic Model. 
+not published.
+```
+
+The manuscript can be found here : [Long Short-Term Memory Networks for Real-time Flood Forecast Correction: A Case Study for an Underperforming Hydrologic Model (TO BE UPDATED)](https://github.com/tug17/ForecastModel)
+
+The code in this repository was used to produce and train all models in our manuscript.
+
+
+## Content of the repository
+
+- `run_preprocessing.py` python file for preprocessing and creating random data
+- `run_tuner.py` python file to train our models
+- `environment.yml` contains installed packeges and dependencies
+- `ForecastModel/` contains the entire code to create, train and tune ARIMA and LSTM models
+   - `ForecastModel/models.py` model architectures code 
+   - `ForecastModel/tuners.py` tuner code 
+   - `ForecastModel/data/` contains code for data model to load samples during training
+   - `ForecastModel/utils/` contains code for metrics and loss calculations, as well as post- and preprocessing functions
+- `data/` containes dataset and sequence index arrays
+- `tb/` contains tuner logs and hyperparameters for tensorboard
+- `final_models/` contains trained models and fold predictions 
+
+## Setup to run the code locally
+
+Download this repository either as zip-file or clone it to your local file system by running
+
+```
+git clone git@github.com:tug17/ForecastModel.git
+```
+
+### Setup Python environment
+Within this repository we provide a environment file (`environment.yml`) that can be used with Anaconda or Miniconda to create an environment with all packages needed.
+Build on Tensorflow 2.10 runs on Windows 10 with a CUDA capable NVIDIA GPU. 
+
+```
+conda env create -f environment.yml
+```
+
+## Data needed
+As original data cannot be published, dummy data will be created randomly during preprocessing.
+
+## Run locally
+
+Activate conda environment:
+
+```
+conda activate tf2
+```
+
+During pre-processing random data is generated and saved to data/. The created index arrays at data/indices are later used do build the sequences for training.
+
+```
+python run_preprocessing.py
+```
+Tuning process is started and logs and fold models are saved to tb/.
+
+```
+python run_tuner.py
+```
+
+
+## Citation
+
+If you use any of this code in your experiments, please make sure to cite the following publication (TO BE UPDATED)
+
+```
+author = {Gegenleithner, S., Manuel Pirker, M., Dorfmann, C., Kern, R., and Schneider, J.},
+title = {Long Short-Term Memory Networks for Real-time Flood Forecast Correction: A Case Study for an Underperforming Hydrologic Model},
+}
+```
+
